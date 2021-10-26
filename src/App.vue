a0c44f6c86759281e74c5148d59dd202cb91f402
zeroyukiy <zeroyukiy@hotmail.com>
2021-10-26 09:19:32 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
+  <Navbar />
+  <Sidebar />
+  <main>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </main>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/Sidebar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar,
+    Sidebar,
   }
 }
 </script>
@@ -21,6 +29,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 6em;
 }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="sidebar">
+        <ul class="menu">
+            <li>Explore</li>
+            <li>Story Board</li>
+            <li>People</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Sidebar",
+}
+</script>
